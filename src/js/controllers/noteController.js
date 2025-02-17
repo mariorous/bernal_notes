096c06deea59dee994eab48693567da2002cbe59
〽️ario <emariors@gmail.com>
2025-02-17 17:19:40 +0100

    changes

---
+++ src/js/controllers/noteController.js
@@ -0,0 +1,23 @@
+import { Note } from "../models/Note.js";
+
+export class NoteController {
+    static createNote(note) {
+        // TODO: Crear una nueva nota
+    }
+
+    static getNotes() {
+        // TODO: Obtener todas las notas
+    }
+
+    static getNote(id) {
+        // TODO: Obtener una nota por su id
+    }
+
+    static updateNote(id, note) {
+        // TODO: Actualizar una nota
+    }
+
+    static deleteNote(id) {
+        // TODO: Eliminar una nota
+    }
+}
